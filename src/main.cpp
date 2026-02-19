be9a4aed6c83783338fb0a3b8259c1eed49d1449
nitinkumar <nitinchaudhary0063@gmail.com>
2026-02-19 21:36:49 +0530

    updated TODO

--- src/main.cpp
+++ src/main.cpp
@@ -10,48 +10,26 @@
  * and analog PWM brightness control
  */
 
- // TODO 1:
- // Define RED pin (Use 9)
 
- // TODO 2:
- // Define GREEN pin (Use 10)
+// TODO 1:
+#define RED_PIN 9
 
- // TODO 3:
- // Define BLUE pin (Use 11)
+// TODO 2:
+#define GREEN_PIN 10
+
+// TODO 3:
+#define BLUE_PIN 11
 
 void setup() {
 
     // TODO 4:
-    // Initialize Serial communication (9600 baud)
+    Serial.begin(9600);
 
     // TODO 5:
-    // Configure RGB pins as OUTPUT
+    pinMode(RED_PIN, OUTPUT);
+    pinMode(GREEN_PIN, OUTPUT);
+    pinMode(BLUE_PIN, OUTPUT);
 
     // TODO 6:
-    // Print initialization message
-}
-
-void loop() {
-
-    // -------- DIGITAL MODE --------
-
-    // TODO 7:
-    // Turn ON red (digital HIGH)
-
-    // TODO 8:
-    // Turn OFF red
-
-    // -------- ANALOG (PWM) MODE --------
-
-    // TODO 9:
-    // Set RED brightness using analogWrite()
-
-    // TODO 10:
-    // Set GREEN brightness using analogWrite()
-
-    // TODO 11:
-    // Set BLUE brightness using analogWrite()
-
-    // TODO 12:
-    // Add delay for visible transition
+    Serial.println("RGB LED System Initialized");
 }
